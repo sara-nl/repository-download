366e60d7254b89e936439df5541120f33efb2729
hansp <hans.vanpiggelen@surfsara.nl>
2018-05-15 16:30:16 +0200

    Update script and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SURF Data Repository basket download script
+# SURF Data Repository download tool
+
+Automated download tool for user basket or favourites.
+
+### General usage
+
+```sh
+./repository-download.py <token>
+```
+
+where `token` is your token generated in the user interface of the Data Repository.
--- repository-download.py
+++ repository-download.py
@@ -60,7 +60,7 @@ except Exception, e:
     error(e, True, 100)
 
 SERVICE = "SURF Data Repository"
-TITLE = SERVICE + " download script"
+TITLE = SERVICE + " download tool"
 VERSION = "1.0-beta"
 
 TDR_API_BASKET = 'api/basket'
@@ -104,7 +104,10 @@ class DownloadManager(object):
         self.cache = {}
         self.files = {}
         self.options = {}
+        self.request = None
+        self.rdata = ""
         self.stageObjects = []
+        self.statusObjects = {}
 
         requests.packages.urllib3.disable_warnings()
 
@@ -155,30 +158,30 @@ class DownloadManager(object):
 
     def _request(self, uri, headers={}, params={}, method='GET'):
         url = "%s/%s" % (self.options['target'], uri) if self.options['target'] not in uri else uri
-        r = requests.request(method, url, headers=headers, params=params, verify=self.options['verify'])
+        self.request = requests.request(method, url, headers=headers, params=params, verify=self.options['verify'])
 
-        debug("%s %s" % (method, r.url))
+        debug("%s %s" % (method, self.request.url))
 
-        if r is None:
+        if self.request is None:
             error('internal request failed', True)
 
         # determine the encoding of the response text
-        if r.encoding is None:
-            r.encoding = cchardet.detect(r.content)['encoding']
+        if self.request.encoding is None:
+            self.request.encoding = cchardet.detect(self.request.content)['encoding']
 
-        if 'content-type' in r.headers:
-            ct = re.search('([a-z]+\/[a-z]+)', r.headers['content-type'], re.I)
+        if 'content-type' in self.request.headers:
+            ct = re.search('([a-z]+\/[a-z]+)', self.request.headers['content-type'], re.I)
             if ct.group(1) in ['application/json', 'text/json']:
-                data = r.json() if r.text > "" else {}
+                self.rdata = self.request.json() if self.request.text > "" else {}
             else:
-                data = r.text
+                self.rdata = self.request.text
 
-        if r.status_code == 200:
-            return data
-        elif r.status_code == 401:
-            error("could not authorize using token", True)
-        elif data is dict and "error" in data:
-            error(data["error"])
+        if self.request.status_code == 200:
+            return self.rdata
+        elif self.request.status_code == 401:
+            error("could not authenticate using token", True)
+        elif self.rdata is dict and "error" in self.rdata:
+            error(self.rdata["error"])
         else:
             return None
 
@@ -252,10 +255,10 @@ class DownloadManager(object):
     # report
     def report(self):
         finished = filter(lambda x: x['dstatus'] == FileStatus.FINISHED, self.files.values())
-        print "Finished: %s" % map(lambda x: x['name'], finished)
+        print "Finished (%d): %s" % (len(finished), map(lambda x: x['name'], finished))
 
         errors = filter(lambda x: x['dstatus'] == FileStatus.ERROR, self.files.values())
-        print "Errors: %s" % map(lambda x: x['name'], errors)
+        print "Errors (%d): %s" % (len(errors), map(lambda x: x['name'], errors))
 
     # initiate download and stage processes
     def startProcesses(self):
@@ -275,10 +278,32 @@ class DownloadManager(object):
                 else:
                     f['dstatus'] = FileStatus.ERROR
             elif f['dstatus'] == FileStatus.STAGING:
-                if self._statusDeposit(f['parent'], f['id']):
-                    f['dstatus'] = FileStatus.DOWNLOAD
-                else:
+                # check if deposit has already been check for status recently
+                if not f['parent'] in self.statusObjects.keys() or time.time() - self.statusObjects[f['parent']]['updated'] > self.options['status-interval']:
+                    self._statusDeposit(f['parent'], f['id'])
+
+                # check if status has changed
+                if not self._processStatusCheck(f):
                     f['dstatus'] = FileStatus.ERROR
+
+    def _processStatusCheck(self, fileObject):
+        # if status has been found
+        if not fileObject['parent'] in self.statusObjects or not self.statusObjects[fileObject['parent']]['result']:
+            return False
+        if 'status_checked' in fileObject and time.time() - fileObject['status_checked'] <= self.options['status-interval']:
+            return True
+
+        debug(' Check status for file %s' % fileObject['name'])
+
+        # find file ID in result and check status
+        for f in self.statusObjects[fileObject['parent']]['result']:
+            if f['id'] == fileObject['id']:
+                if f['status'] in STATUS_ONLINE:
+                    f['dstatus'] = FileStatus.DOWNLOAD
+
+        fileObject['status_checked'] = time.time()
+
+        return True
 
     def _processDownloads(self):
         for (i, f) in enumerate(self.files.itervalues()):
@@ -299,22 +324,31 @@ class DownloadManager(object):
     # download a file and compare the checksum
     def _downloadFile(self, fileObject):
         # request streaming data
-        r = requests.get(fileObject['url'], params={'token': self.token}, stream=True, verify=self.options['verify'])
+        self.request = requests.get(fileObject['url'], params={'token': self.token}, stream=True, verify=self.options['verify'])
 
-        if r.status_code != 200:
-            error('download of file %s failed: %s' % (fileObject['name'], r.reason))
+        if self.request.status_code != 200:
+            error('download of file %s failed: %s' % (fileObject['name'], self.request.reason))
             return False
 
-        debug("%s %s" % (r.request.method, r.url))
+        debug("%s %s" % (self.request.request.method, self.request.url))
 
         # get file size
-        filesize = int(r.headers.get('content-length')) if 'content-length' in r.headers else 1
+        filesize = int(self.request.headers.get('content-length')) if 'content-length' in self.request.headers else 1
         totsize = 0
 
+        # create the output dir
+        targetdir = "%s/%s" % (self.options['outputdir'], fileObject['parent'].replace(':', '/'))
+        try:
+            if not os.path.exists(targetdir):
+                os.makedirs(targetdir)
+        except OSError, e:
+            error(e, True, 101)
+
         # download the file in chunks
-        with open("%s/%s" % (self.options['outputdir'], fileObject['name']), 'wb') as fout:
+        fileObject['target'] = "%s/%s" % (targetdir, fileObject['name'])
+        with open(fileObject['target'], 'wb') as fout:
             start = time.clock()
-            for chunk in r.iter_content(chunk_size=Config.BUFFER_SIZE):
+            for chunk in self.request.iter_content(chunk_size=Config.BUFFER_SIZE):
                 if chunk:
                     size = len(chunk)
                     totsize += size
@@ -327,9 +361,9 @@ class DownloadManager(object):
                 # print progress
                 progress = "(%d%%, %s, %s/s)" % (totsize * 100. / filesize, bytesize(totsize), bytesize(int(size / elapsed))) if filesize else ""
                 if totsize == filesize:
-                    sys.stdout.write('\r' + "file %s downloaded %s" % (fileObject['name'], progress))
+                    sys.stdout.write("\rFile %s downloaded %s" % (fileObject['name'], progress))
                 else:
-                    sys.stdout.write('\r' + "downloading file %s %s" % (fileObject['name'], progress))
+                    sys.stdout.write("\rDownloading file %s %s" % (fileObject['name'], progress))
                 sys.stdout.flush()
 
             sys.stdout.write('\n')
@@ -338,11 +372,14 @@ class DownloadManager(object):
 
     # check the checksum of a downloaded file
     def _checksumFile(self, fileObject):
-        text = "comparing local checksum of file '%s'"
+        text = "Comparing local checksum of file '%s'"
         sys.stdout.write(text % fileObject['name'])
 
         # calculate the local checksum
-        output = subprocess.check_output("md5sum %s/%s | awk '{print $1}'" % (self.options['outputdir'], fileObject['name']), shell=True).strip()
+        try:
+            output = subprocess.check_output("md5sum %s | awk '{print $1}'" % (fileObject['target']), shell=True).strip()
+        except subprocess.CalledProcessError, e:
+            error(e, True, 102)
 
         # compare with retrieved checksum
         if output == fileObject['md5']:
@@ -350,36 +387,49 @@ class DownloadManager(object):
         else:
             sys.stdout.write("\r" + text % fileObject['name'] + " FAIL (%s vs %s)" % (output, fileObject['md5']))
 
+        # store checksum if requested
+        if self.options['store-checksum']:
+            with open("%s.md5" % fileObject['target'], 'w') as f:
+                f.write(output)
+                f.close()
+
         sys.stdout.write('\n')
 
-        return True
+        return output == fileObject['md5']
 
     def _stageDeposit(self, pid):
         # check if deposit has already been staged
         if pid in self.stageObjects:
             return True
 
+        print 'Staging %s' % pid
+
         # request stage
         pids = pid.split(':')
         success = self._requestAuthenticated(TDR_API_STAGE % (pids[0], pids[1]))
         if not success:
-            error('object staging failed: %s' % pid)
+            error('object staging of %s failed (%s): %s' % (pid, self.request.status_code, self.rdata['error']))
             return False
 
-        debug('staging %s' % pid)
         self.stageObjects.append(pid)
 
         return True
 
     def _statusDeposit(self, pid, fileID=None):
+        print 'Status %s' % pid
+
         pids = pid.split(':')
         success = self._requestAuthenticated(TDR_API_STATUS % (pids[0], pids[1]))
         if not success:
-            error('object status failed: %s' % pid)
+            error('object status of %s failed (%s): %s' % (pid, self.request.status_code, self.rdata['error']))
             return False
 
-        debug('status %s' % pid)
+        self.statusObjects.update({pid: {'updated': time.time(), 'result': self.rdata}})
+
         return True
+
+def bools(b):
+    return "yes" if b else "no"
 
 def usage(options):
     """ Show usage information """
@@ -387,21 +437,24 @@ def usage(options):
     print "Syntax: %s token [options]" % sys.argv[0]
     print "\n where token is your personal access token"
     print "\nOptions:"
-    print "--target         -t  set the target instance of %s" % SERVICE
-    print "--favourites     -f  download favourites instead of basket (default: %d)" % options['favourites']
-    print "--output         -o  set target download directory (default: %d)" % options['outputdir']
-    print "--buffer-size    -b  set download buffer size (bytes, default: %d)" % Config.BUFFER_SIZE
-    print "--skip-checksum  -s  skip checksum check after download (default: %d)" % options['checksum']
-    print "--no-verify          do not verify server certificate (default: %d)" % options['verify']
-    print "--version            prints '%s %s'" % (TITLE, VERSION)
-    print "--debug          -d  set debuglevel to max"
-    print "--help           -h  help mode"
-    print "                 -v  verbose mode"
+    print "--target          -t  set the target instance of %s (default: '%s')" % (SERVICE, BASE_URL)
+    print "--favourites      -f  download favourites instead of basket (default: %s)" % bools(options['favourites'])
+    print "--output          -o  set target download directory (default: '%s')" % options['outputdir']
+    print "--buffer-size     -b  set download buffer size (bytes, default: %d)" % Config.BUFFER_SIZE
+    print "--skip-checksum   -s  skip checksum check after download (default: %s)" %  bools(not options['check-checksum'])
+    print "--store-checksum  -s  store generated checksum after download (default: %s)" % bools(options['store-checksum'])
+    print "--verify              verify server certificate (default: %s)" % bools(options['verify'])
+    print "--version             prints '%s %s'" % (TITLE, VERSION)
+    print "--debug           -d  set debuglevel to max"
+    print "--help            -h  help mode"
+    print "                  -v  verbose mode"
 
 # parse arguments
 def main(argv):
     options = {
-        'checksum': True,
+        'check-checksum': True,
+        'store-checksum': False,
+        'status-interval': 30,
         'favourites': False,
         'outputdir': "download",
         'target': BASE_URL,
@@ -411,7 +464,7 @@ def main(argv):
     # handle arguments
     try:
         opts, args = getopt.gnu_getopt(argv, "hdvo:vfs",  \
-            ["help", "version", "debug", "verbose", "output=", "target=", "no-verify", "favourites", "skip-checksum"])
+            ["help", "version", "debug", "verbose", "output=", "target=", "verify", "favourites", "skip-checksum", "store-checksum"])
     except getopt.GetoptError as err:
         error(err, True)
 
@@ -427,10 +480,12 @@ def main(argv):
         elif opt == "--version":
             print "%s %s" % (TITLE, VERSION)
             sys.exit(1)
-        elif opt == "--no-verify":
+        elif opt == "--verify":
             options.update({'verify': False})
         elif opt in ("-s", "--skip-checksum"):
-            options.update({'checksum': False})
+            options.update({'check-checksum': False})
+        elif opt in ("--store-checksum"):
+            options.update({'store-checksum': True})
         elif opt in ("-f", "--favourites"):
             options.update({'favourites': True})
         elif opt in ("-d", "--debug"):
